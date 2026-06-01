ec0f82fbb1b2518ea94c58f845aedd600fd5930e
Sriram Akhil <sreeramakhil@users.noreply.github.com>
2026-06-01 11:18:18 +0530

    feat: enable wildcard HTTP/HTTPS origins in CORS to support Vercel deployment

--- backend/app.py
+++ backend/app.py
@@ -21,8 +21,7 @@ app.config.update(
 )
 
 CORS(app, supports_credentials=True, origins=[
-    "http://127.0.0.1:8080", "http://localhost:8080", "http://192.168.0.104:8080",
-    "http://127.0.0.1:5500", "http://localhost:5500", "http://192.168.0.104:5500"
+    r"https?://.*"
 ])
 
 # ─── Connect to Blockchain (Local Ganache or Sepolia Cloud) ───────────────────
